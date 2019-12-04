6d8022c31c6325390a336fe0043f6fd61ed0a545
Zach Leatherman <zachleatherman@gmail.com>
2019-12-04 14:45:26 -0600

    Allow resize even if browser doesn’t support ResizeObserver

--- resizeasaurus.js
+++ resizeasaurus.js
@@ -11,30 +11,16 @@ if( "customElements" in window) {
       if(!this.size) {
         this.size = document.createElement("div");
         this.size.classList.add("resizeasaurus-size");
+        this.size.textContent = "Drag to resize";
         this.appendChild(this.size); 
       }
 
-      this.resizer = new ResizeObserver(entries => {
-        this.size.innerHTML = this.clientWidth + "px";
-      });
-      this.resizer.observe(this);
-
-      this.cacheWidth();
-    }
-
-    cacheWidth(counter = 0) {
-      requestAnimationFrame(() => {
-        if(this.clientWidth) {
-          this.lastKnownWidth = this.clientWidth;
-        } else if(counter < 20) {
-          this.cacheWidth(++counter);
-        } else {
-          // back it off to every 100ms
-          setTimeout(() => {
-            this.cacheWidth(++counter);
-          }, 100);
-        }
-      });
+      if("ResizeObserver" in window) {
+        this.resizer = new ResizeObserver(entries => {
+          this.size.innerHTML = this.clientWidth + "px";
+        });
+        this.resizer.observe(this);
+      }
     }
 
     resize() {
